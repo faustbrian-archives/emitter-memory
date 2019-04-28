6b06899bbaadb0b85a3d44df9153dc298544a182
Brian Faust <hello@basecode.sh>
2019-04-28 07:54:30 +0300

    naming

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,57 @@
 export type EventName = string | symbol;
-export type EventHandler = (eventData: any) => void;
-export type WildcardEventHandler = (eventName: EventName, eventData: any) => void;
+export type EventHandler = (data: any) => void;
+export type WildcardEventHandler = (name: EventName, data: any) => void;
 
-/**
- * @TODO
- * - add emitSync / emitSeqSync
- */
 export class Evento {
 	private readonly listenersWildcard: Set<WildcardEventHandler> = new Set<WildcardEventHandler>();
 	private readonly listenersEvent: Map<EventName, Set<EventHandler>> = new Map<EventName, Set<EventHandler>>();
 
-	public on(eventName: EventName, listener: EventHandler): () => void {
-		this.listeners(eventName).add(listener);
+	public on(name: EventName, listener: EventHandler): () => void {
+		this.listeners(name).add(listener);
 
-		return this.off.bind(this, eventName, listener);
+		return this.off.bind(this, name, listener);
 	}
 
-	public off(eventName: EventName, listener: EventHandler): void {
-		this.listeners(eventName).delete(listener);
+	public off(name: EventName, listener: EventHandler): void {
+		this.listeners(name).delete(listener);
 	}
 
-	public once(eventName: EventName, listener: EventHandler): void {
-		const off: () => void = this.on(eventName, data => {
+	public once(name: EventName, listener: EventHandler): void {
+		const off: () => void = this.on(name, data => {
 			off();
 
 			listener(data);
 		});
 	}
 
-	public async emit(eventName: EventName, eventData?: any): Promise<void> {
+	public async emit(name: EventName, data?: any): Promise<void> {
 		await Promise.resolve();
 
 		await Promise.all([
-			Array.from(this.listeners(eventName)).map((listener: EventHandler) => listener(eventData)),
-			Array.from(this.listenersWildcard).map((listener: WildcardEventHandler) => listener(eventName, eventData)),
+			Array.from(this.listeners(name)).map((listener: EventHandler) => listener(data)),
+			Array.from(this.listenersWildcard).map((listener: WildcardEventHandler) => listener(name, data)),
 		]);
 	}
 
-	public async emitSeq(eventName: EventName, eventData?: any): Promise<void> {
+	public async emitSeq(name: EventName, data?: any): Promise<void> {
 		await Promise.resolve();
 
-		for (const listener of this.listeners(eventName).values()) {
-			await listener(eventData);
+		for (const listener of this.listeners(name).values()) {
+			await listener(data);
 		}
 
 		for (const listener of this.listenersWildcard.values()) {
-			await listener(eventName, eventData);
+			await listener(name, data);
 		}
 	}
 
-	public emitSync(eventName: EventName, eventData?: any): void {
-		for (const listener of this.listeners(eventName).values()) {
-			listener(eventData);
+	public emitSync(name: EventName, data?: any): void {
+		for (const listener of this.listeners(name).values()) {
+			listener(data);
 		}
 
 		for (const listener of this.listenersWildcard.values()) {
-			listener(eventName, eventData);
+			listener(name, data);
 		}
 	}
 
@@ -69,18 +65,18 @@ export class Evento {
 		this.listenersWildcard.delete(listener);
 	}
 
-	public clearListeners(eventName?: EventName): void {
-		if (eventName) {
-			this.listeners(eventName).clear();
+	public clearListeners(name?: EventName): void {
+		if (name) {
+			this.listeners(name).clear();
 		} else {
 			this.listenersWildcard.clear();
 			this.listenersEvent.clear();
 		}
 	}
 
-	public listenerCount(eventName?: EventName): number {
-		if (eventName) {
-			return this.listenersWildcard.size + this.listeners(eventName).size;
+	public listenerCount(name?: EventName): number {
+		if (name) {
+			return this.listenersWildcard.size + this.listeners(name).size;
 		}
 
 		let count: number = this.listenersWildcard.size;
@@ -92,12 +88,12 @@ export class Evento {
 		return count;
 	}
 
-	public listeners(eventName: EventName): Set<EventHandler> {
-		if (!this.listenersEvent.has(eventName)) {
-			this.listenersEvent.set(eventName, new Set());
+	public listeners(name: EventName): Set<EventHandler> {
+		if (!this.listenersEvent.has(name)) {
+			this.listenersEvent.set(name, new Set());
 		}
 
-		return this.listenersEvent.get(eventName);
+		return this.listenersEvent.get(name);
 	}
 
 	public eventNames(): EventName[] {
