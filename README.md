712f6c53ce92302d1c94322b226e06f5b8dd7370
Brian Faust <hello@basecode.sh>
2020-12-16 12:51:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# evento
+
+<p align="center"><img src="./banner.png" /></p>
+
+[![Latest Version](https://badgen.now.sh/npm/v/@konceiver/emitter-memory)](https://www.npmjs.com/package/@konceiver/emitter-memory)
+[![License: MIT](https://badgen.now.sh/badge/license/MIT/green)](./LICENSE)
+[![Build Status](https://img.shields.io/github/workflow/status/konceiver/emitter-memory/run-tests?label=tests)](https://img.shields.io/github/workflow/status/konceiver/emitter-memory/CI?label=CI)
+[![Total Downloads](https://badgen.net/npm/dt/konceiver/emitter-memory)](https://npmjs.org/package/@konceiver/emitter-memory)
+
+## Installation
+
+```bash
+yarn add @faustbrian/evento
+```
+
+## Testing
+
+```bash
+yarn test
+```
+
+## Security
+
+If you discover a security vulnerability within this package, please send an e-mail to security@konceiver.dev. All security vulnerabilities will be promptly addressed.
+
+## Credits
+
+This project exists thanks to all the people who [contribute](../../contributors).
+
+## License
+
+This is free and open source software under the terms of the [MIT License](./LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,106 @@
+export type EventName = string | symbol;
+export type EventHandler = (data: any) => void;
+export type WildcardEventHandler = (name: EventName, data: any) => void;
+
+export class Evento {
+	private readonly listenersWildcard: Set<WildcardEventHandler> = new Set<WildcardEventHandler>();
+	private readonly listenersEvent: Map<EventName, Set<EventHandler>> = new Map<EventName, Set<EventHandler>>();
+
+	public on(name: EventName, listener: EventHandler): () => void {
+		this.listeners(name).add(listener);
+
+		return this.off.bind(this, name, listener);
+	}
+
+	public off(name: EventName, listener: EventHandler): void {
+		this.listeners(name).delete(listener);
+	}
+
+	public once(name: EventName, listener: EventHandler): void {
+		const off: () => void = this.on(name, data => {
+			off();
+
+			listener(data);
+		});
+	}
+
+	public async emit(name: EventName, data?: any): Promise<void> {
+		await Promise.resolve();
+
+		await Promise.all([
+			Array.from(this.listeners(name)).map((listener: EventHandler) => listener(data)),
+			Array.from(this.listenersWildcard).map((listener: WildcardEventHandler) => listener(name, data)),
+		]);
+	}
+
+	public async emitSeq(name: EventName, data?: any): Promise<void> {
+		await Promise.resolve();
+
+		for (const listener of this.listeners(name).values()) {
+			await listener(data);
+		}
+
+		for (const listener of this.listenersWildcard.values()) {
+			await listener(name, data);
+		}
+	}
+
+	public emitSync(name: EventName, data?: any): void {
+		for (const listener of this.listeners(name).values()) {
+			listener(data);
+		}
+
+		for (const listener of this.listenersWildcard.values()) {
+			listener(name, data);
+		}
+	}
+
+	public onAny(listener: WildcardEventHandler): () => void {
+		this.listenersWildcard.add(listener);
+
+		return this.offAny.bind(this, listener);
+	}
+
+	public offAny(listener: WildcardEventHandler): void {
+		this.listenersWildcard.delete(listener);
+	}
+
+	public clearListeners(name?: EventName): void {
+		if (name) {
+			this.listeners(name).clear();
+		} else {
+			this.listenersWildcard.clear();
+			this.listenersEvent.clear();
+		}
+	}
+
+	public listenerCount(name?: EventName): number {
+		if (name) {
+			return this.listenersWildcard.size + this.listeners(name).size;
+		}
+
+		let count: number = this.listenersWildcard.size;
+
+		for (const value of this.listenersEvent.values()) {
+			count += value.size;
+		}
+
+		return count;
+	}
+
+	public listeners(name: EventName): Set<EventHandler> {
+		if (!this.listenersEvent.has(name)) {
+			this.listenersEvent.set(name, new Set());
+		}
+
+		return this.listenersEvent.get(name);
+	}
+
+	public rawListeners(name: EventName): EventHandler[] {
+		return [...this.listeners(name)];
+	}
+
+	public eventNames(): EventName[] {
+		return [...this.listenersEvent.keys()];
+	}
+}
